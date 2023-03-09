df6934317e17f8997216ba7722096866908f396a
James Lyerla <lyerlajd@gmail.com>
2023-03-08 18:09:32 -0600

    switching back to desktop

--- Angular-Demo/src/app/app.module.ts
+++ Angular-Demo/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule}  from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   // for self-made components
@@ -30,7 +31,8 @@ import { MatInputModule}  from '@angular/material/input';
     FormsModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
